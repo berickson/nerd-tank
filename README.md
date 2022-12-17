680aacc3a62d97b23587370a7bb66710d7267b41
Brian Erickson <berickson@gmail.com>
2022-12-16 23:29:27 -0800

    Temp probe working

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # nerd-tank
 Beginnings of an aquarium controller.  
 Targets an Heltec ESP-32 Wifi Kit R3.
+Temperature Sensor DS18B20
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
+
+// for the OLED Display
 #include "SSD1306Wire.h"
 
+// for the DS18B20 Temperature Sensor
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 // oled display
 const int oled_address=0x3c;
 const int pin_oled_sda = 17;
 const int pin_oled_scl = 18;
 const int pin_oled_rst = 21;
+
+// temperature sensor pin
+const int pin_one_wire_bus = 26;
+OneWire one_wire(pin_one_wire_bus);
+DallasTemperature temperature_probe(&one_wire);
 
 SSD1306Wire display(oled_address, pin_oled_sda, pin_oled_scl);
 
@@ -53,6 +63,10 @@ void setup() {
   init_display();
   Serial.println("End of setup");
   scan_i2c_addresses();
+  pinMode(26, INPUT_PULLUP);
+  temperature_probe.begin();
+  pinMode(26, INPUT_PULLUP);
+  temperature_probe.setResolution(12);
   
 }
 
@@ -61,25 +75,101 @@ bool every_n_ms(unsigned long last_loop_ms, unsigned long loop_ms, unsigned long
   return (last_loop_ms % ms) + (loop_ms - last_loop_ms) >= ms;
 }
 
+class Statistics {
+  public:
+      int count = 0;
+      float sum_x = 0.0;
+      float sum_x2 = 0.0;
+
+
+
+    void reset() {
+      count = 0;
+      sum_x = 0.0;
+      sum_x2 = 0.0;
+    }
+
+    void add_reading(float x) {
+      count += 1;
+      sum_x += x;
+      sum_x2 += x*x;
+    }
+
+    float mean() {
+      return sum_x / count;
+    }
+};
+
 void loop() {
   static unsigned long loop_ms = 0;
   static unsigned long last_loop_ms = 0;
   static int64_t loop_count = 0;
+  static Statistics probe_0_statistics;
   last_loop_ms = loop_ms;
   loop_ms = millis();
   ++loop_count;
+  static float mean_temp_f = NAN;
+
+
+  // get temperature
+  if(every_n_ms(last_loop_ms, loop_ms, 1000)) {
+    temperature_probe.setWaitForConversion(false);
+    temperature_probe.requestTemperatures(); // Send the command to get temperatures
+  }
 
 
   if(every_n_ms(last_loop_ms, loop_ms, 1000)) {
-    Serial.print("Loops: ");
-    Serial.print(loop_count);
-    Serial.println();
+    float temp_f = temperature_probe.getTempFByIndex(0);
+    probe_0_statistics.add_reading(temp_f);
+    if(probe_0_statistics.count >= 30) {
+      mean_temp_f = probe_0_statistics.mean();
+      Serial.print("Probe 0 mean: ");
+      Serial.println(mean_temp_f);
+      probe_0_statistics.reset();
+    }
+
 
     display.clear();
     char buff[80];
     sprintf(buff, "loop_count - %d", loop_count);
     display.drawString(0, 0, buff);
+    sprintf(buff, "degrees F: %.1f", mean_temp_f);
+    display.drawString(0, 10, buff);
     display.display();
+
+
+  }
+  if(every_n_ms(last_loop_ms, loop_ms, 10000)) {
+
+    Serial.print("Loops: ");
+    Serial.print(loop_count);
+
+    auto temp_probe_count = temperature_probe.getDS18Count();
+    Serial.print(" df18 count: " );
+    Serial.print(temp_probe_count);
+
+    Serial.print(" temp_f: ");
+
+    for(auto i=0;i<temp_probe_count; ++i) {
+      Serial.print(" ");
+      Serial.print(temperature_probe.getTempFByIndex(i));
+    }
+
+
+    Serial.print(" addresses: ");
+    for(auto i=0;i<temp_probe_count; ++i) {
+      DeviceAddress address = {0,0,0,0,0,0,0,0};
+      if(temperature_probe.getAddress(address,i)) {
+        Serial.print(" ");
+        for(auto j=0; j<8; ++j) {
+          Serial.print(" ");
+          Serial.print(address[j],HEX);
+        }
+      }
+    }
+
+    Serial.println();
+
   }
   
 }
